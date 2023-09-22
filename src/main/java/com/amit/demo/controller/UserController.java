c0857827d7e49b0f837873c7f6820e6042c114f8
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-09-22 18:55:26 +0530

    adding getmapping

--- src/main/java/com/amit/demo/controller/UserController.java
+++ src/main/java/com/amit/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,37 +26,9 @@ import com.amit.demo.service.UserService;
 @CrossOrigin(origins ="*")
 public class UserController {
 	
-	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
-		UserDto createUser = this.userService.createUser(userDto);
-		return new ResponseEntity<UserDto>(createUser,HttpStatus.CREATED);
+	@GetMapping
+	ResponseEntity<String> methodTesting(){
+		return new ResponseEntity<String>(new String("put mapping is working"),HttpStatus.OK);
 	}
 	
-	@GetMapping("/{userId}")
-	public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId){
-		UserDto user = this.userService.getUser(userId);
-
-		return new ResponseEntity<UserDto>(user,HttpStatus.OK);
-	}
-	
-	@GetMapping("/")
-	public ResponseEntity<List<UserDto>> getAllUser(){
-		System.out.println("i have been called");
-		List<UserDto> allUser = this.userService.getAllUser();
-		
-		return new ResponseEntity<List<UserDto>>(allUser,HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/{userId}")
-	public ResponseEntity<?> deleteUser(@PathVariable Integer userId){
-		this.userService.deleteUser(userId);
-		Map.Entry<String, String> entry = Map.entry("message ", " user is deleted successfully ");
-		return new ResponseEntity<>(entry,HttpStatus.OK);
-	}
-	
-	
-
 }
